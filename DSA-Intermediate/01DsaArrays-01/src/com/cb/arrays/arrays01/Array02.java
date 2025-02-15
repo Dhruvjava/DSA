9da974f6c8ffaba436161347e1e7379114f65f75
Dhruv Kumar <d.k.dhakarey1999@gmail.com>
2025-02-15 11:08:24 +0530

    sum of pair of two index equal to k exists or not

---
+++ DSA-Intermediate/01DsaArrays-01/src/com/cb/arrays/arrays01/Array02.java
@@ -0,0 +1,23 @@
+package com.cb.arrays.arrays01;
+
+public class Array02 {
+
+    public static void main(String[] args) {
+        int[] a = new int[] {3, -2, 1, 4, 3, 6, 8};
+        int k = 10;
+        boolean pairExists = sumOfTwoPairsExists(a, k);
+        System.out.println("Sum of Pains equals to k is exists : " + pairExists);
+    }
+
+    public static boolean sumOfTwoPairsExists(int[] arr, int k) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && arr[i] + arr[j] == k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
