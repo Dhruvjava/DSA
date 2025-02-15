29d562cea2a1efc01d874d3144a0e8b737777ea3
Dhruv Kumar <d.k.dhakarey1999@gmail.com>
2025-02-15 10:20:51 +0530

    Optimized Time Complexity from o(n2) to O(n)

--- DSA-Intermediate/01DsaArrays-01/src/com/cb/arrays/arrays01/Array01.java
+++ DSA-Intermediate/01DsaArrays-01/src/com/cb/arrays/arrays01/Array01.java
@@ -2,22 +2,25 @@ package com.cb.arrays.arrays01;
 
 public class Array01 {
     public static void main(String[] args) {
-        int[] arr = new int[] {-3, -2, 6, 8, 4, 8, 5};
+        int[] arr = new int[] {2, 5, 1, 4, 8, 0, 8, 1, 3, 8};
 
         int count = countOfGreateEle(arr);
 
         System.out.println("Number of count is :" + count);
     }
 
-    public static int countOfGreateEle(int[] arr){
+    public static int countOfGreateEle(int[] arr) {
         int n = arr.length;
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[j] > arr[i]) {
-                    count++;
-                    break;
-                }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0;i<n;i++){
+            if (max < arr[i]){
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++){
+            if (arr[i] == max){
+                count++;
             }
         }
         return count;
